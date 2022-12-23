e2234ec0b0cb3ca74b9a8934f704603f4c3a922f
dadadon <davianberoni@yahoo.com>
2022-12-23 09:39:14 -0500

    Create MyList class

--- myList.rb
+++ myList.rb
@@ -16,4 +16,18 @@ module MyEnumerable
     end
 end
   
-
+class MyList
+    include MyEnumerable
+  
+    def initialize
+      @list = []
+    end
+  
+    def append(el)
+      @list << el
+    end
+  
+    def each
+      @list.each { |el| yield el }
+    end
+end
