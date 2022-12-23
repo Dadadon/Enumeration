c6e05d087a93ef57eef4e3aba8091afc9ad8da73
dadadon <davianberoni@yahoo.com>
2022-12-23 09:53:39 -0500

    Update file

--- myList.rb
+++ myList.rb
@@ -1,33 +1,17 @@
-module MyEnumerable
-    def all?
-      each { |el| return false unless yield el }
-      true
-    end
-  
-    def any?
-      each { |el| return true if yield el }
-      false
-    end
-  
-    def filter
-      result = []
-      each { |el| result << el if yield el }
-      result
-    end
-end
-  
+require './myEnumerable'
+
 class MyList
-    include MyEnumerable
+  include MyEnumerable
   
-    def initialize
-      @list = []
-    end
+  def initialize
+    @list = []
+  end
   
-    def append(el)
-      @list << el
-    end
+  def append(el)
+    @list << el
+  end
   
-    def each
-      @list.each { |el| yield el }
-    end
+  def each
+    @list.each { |el| yield el }
+  end
 end
