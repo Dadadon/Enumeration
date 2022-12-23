94d884f6c946143fbf26162c07e3b6e5ea0d036e
dadadon <davianberoni@yahoo.com>
2022-12-23 09:38:13 -0500

    Create MyEnumerable class and implement functions

---
+++ myList.rb
@@ -0,0 +1,19 @@
+module MyEnumerable
+    def all?
+      each { |el| return false unless yield el }
+      true
+    end
+  
+    def any?
+      each { |el| return true if yield el }
+      false
+    end
+  
+    def filter
+      result = []
+      each { |el| result << el if yield el }
+      result
+    end
+end
+  
+
